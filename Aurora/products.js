06dcfee06066730bc3e9230dc653217f3267d432
ivanParat <ivan.parat2003@gmail.com>
2023-12-17 15:54:37 +0100

    some products added

---
+++ Aurora/products.js
@@ -0,0 +1,23 @@
+// class product{
+//     constructor(name, brand, price, category, imageSrc){
+//         this.name=name;
+//         this.brand=brand;
+//         this.price=price;
+//         this.category=category;
+//         this.imageSrc=imageSrc;
+//     }
+//     addToLocalStorage(){
+//         localStorage.setItem(this.localStorageName, JSON.stringify({email:this.email, username:this.username, password:this.password} ));
+//     }
+// }
+
+// fetch('./products.json')
+// .then(response => response.json())
+// .then(data => {
+
+// const jsonString = JSON.stringify(data);
+
+
+// localStorage.setItem('products', jsonString);
+// })
+// .catch(error => console.error('Error fetching JSON data:', error));
